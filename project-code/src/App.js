bddcf90dc93204a6be65f40228584c21c0b9abf8
vaiibhavii <vaibhskulk@gmail.com>
2024-10-06 12:35:31 +0530

    Added Navbar JSX and CSS files

--- project-code/src/App.js
+++ project-code/src/App.js
@@ -2,9 +2,11 @@ import './App.css';
 
 import Homepage from './Components/Homepage';
 import Footer from './Components/Footer';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (<div className="App">
+    <Navbar />
     <Homepage />
     <Footer />
   </div>);
---
+++ project-code/src/Components/Navbar.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark">
+      <div className="container">
+        {/* Logo */}
+        <a className="navbar-brand" href="/">
+          <img src="https://placehold.co/150x50?text=Logo+Here" alt="Logo" />
+        </a>
+
+        {/* Navbar Toggler for mobile view */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Center Links */}
+        <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item d-flex align-items-center justify-content-center">
+              <a className="nav-link" href="/home">Home</a>
+            </li>
+            <li className="nav-item d-flex align-items-center justify-content-center">
+              <a className="nav-link" href="/about">About</a>
+            </li>
+            <li className="nav-item d-flex align-items-center justify-content-center">
+              <a className="nav-link" href="/contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+
+        {/* Right Buttons */}
+        <div className="d-flex">
+          <button className="btn btn-login me-2" type="button">Log In</button>
+          <button className="btn btn-create-account" type="button">Create Account</button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
